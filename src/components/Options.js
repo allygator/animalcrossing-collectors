b1698a901b3dcf1b0edb11606a604c46ac2f9a76
Alyssa <allygator95@gmail.com>
2020-04-29 21:41:40 -0400

    Change button wording

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,32 +1,47 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 function Options(props) {
-    const options = ['Bugs', 'Fish', 'All'];
+  const options = ["Bugs", "Fish", "Both"];
 
-    return (<ButtonGroup size="medium" color="primary" variant="contained" aria-label="outlined primary button group">
-        {
-            options.map((option, index) => (
-                <Button onClick={() => {
-                    props.setType(index + 1);
-                    props.toggleLoading(true);
-                    props.date ? props.submit(true) : props.submit(false);
-                }} key={option}>
-                {
-                    index === 2
-                        ? ""
-                        : (props.type ? '' : <span className="reduce">Available&nbsp;</span>)
-                }
-                {option}
-                {
-                    index === 2
-                        ? (props.type ? '' : <span className="reduce">&nbsp;Available</span>)
-                        : ''
-                }
-            </Button>))
-        }
-    </ButtonGroup>);
+  return (
+    <ButtonGroup
+      size="medium"
+      color="primary"
+      variant="contained"
+      aria-label="outlined primary button group"
+    >
+      {options.map((option, index) => (
+        <Button
+          onClick={() => {
+            props.setType(index + 1);
+            props.toggleLoading(true);
+            props.date ? props.submit(true) : props.submit(false);
+          }}
+          key={option}
+        >
+          {index === 2 ? (
+            ""
+          ) : props.type ? (
+            ""
+          ) : (
+            <span className="reduce">Available&nbsp;</span>
+          )}
+          {option}
+          {index === 2 ? (
+            props.type ? (
+              ""
+            ) : (
+              <span className="reduce">&nbsp;Available</span>
+            )
+          ) : (
+            ""
+          )}
+        </Button>
+      ))}
+    </ButtonGroup>
+  );
 }
 
 export default Options;
